876c616bc7a50acd3563edcf29c8dfbf1615550c
Paul Schifferer <paul@schifferers.net>
2021-09-17 21:06:04 -0700

    Requirements

--- src/sweetrpg_api_common/__init__.py
+++ src/sweetrpg_api_common/__init__.py
@@ -3,13 +3,13 @@ __author__ = "Paul Schifferer <dm@sweetrpg.com>"
 """
 """
 
-__title__ = 'PACKAGE_NAME'
-__description__ = 'PACKAGE_DESC'
-__url__ = 'PACKAGE_URL'
+__title__ = 'SweetRPG API Common'
+__description__ = 'Common code for API microservice applications'
+__url__ = 'https://github.com/sweetrpg/api-common'
 __version__ = '0.0.0'
 __build__ = 0x000000
-__author__ = 'PACKAGE_AUTHOR'
-__author_email__ = 'PACKAGE_AUTHOR_EMAIL'
+__author__ = 'Paul Schifferer'
+__author_email__ = 'dm@sweetrpg.com'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2021 SweetRPG'
 __cake__ = u'\u2728 \U0001f370 \u2728'
