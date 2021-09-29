e4153593d424c7ff4fb258619fcb88b0289bed59
Paul Schifferer <paul@schifferers.net>
2021-09-28 17:43:36 -0700

    Workflow updates. Add coverage badge.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
-    name="SweetRPG API Core",
+    name="sweetrpg-api-core",
     install_requires=[
         "Flask>=2.0",
         "sweetrpg-db @ git+https://github.com/sweetrpg/db.git@develop"
