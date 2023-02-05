eb0f518c1be9eaae9e6e192d096cec6830782d57
Paul Schifferer <paul@schifferers.net>
2023-02-05 11:45:32 -0800

    Requirements and test versions

--- scripts/update-requirements.sh
+++ scripts/update-requirements.sh
@@ -7,8 +7,11 @@ scriptdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 pushd ${scriptdir}/..
 
 for r in pkg docs tests dev; do
+    echo "--------------------------"
     echo "Requirement: $r"
     pip-compile -r -U requirements/$r.in
+    echo "--------------------------"
+    echo ""
 done
 
 popd
