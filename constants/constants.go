988b4e16374074495838752d885c31f753012f85
Paul Schifferer <paul@schifferers.net>
2024-10-31 13:34:47 -0700

    Add port and env constant

--- constants/constants.go
+++ constants/constants.go
@@ -3,18 +3,20 @@ package constants
 // Environment variable names
 const (
 	BIND_ADDRESS    = "BIND_ADDRESS"
+	PORT            = "PORT"
+	ENV             = "ENV"
 	REDIS_DB        = "REDIS_DB"
 	REDIS_HOST      = "REDIS_HOST"
 	REDIS_PASS      = "REDIS_PASS"
 	REDIS_PORT      = "REDIS_PORT"
 	SENTRY_DSN      = "SENTRY_DSN"
-	SENTRY_DEBUG      = "SENTRY_DEBUG"
+	SENTRY_DEBUG    = "SENTRY_DEBUG"
 	TRACING_NAME    = "TRACING_NAME"
 	ZIPKIN_ENDPOINT = "ZIPKIN_ENDPOINT"
 )
 
 // Value constants
 const (
-    PageStartOption = "start"
-    PageLimitOption = "limit"
+	PageStartOption = "start"
+	PageLimitOption = "limit"
 )
