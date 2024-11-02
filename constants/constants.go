b35fbc61bffcb9cc1b1ec8aee16578fcd48157f2
Paul Schifferer <paul@schifferers.net>
2024-11-02 09:38:41 -0700

    Add constants for ingress info

--- constants/constants.go
+++ constants/constants.go
@@ -2,18 +2,21 @@ package constants
 
 // Environment variable names
 const (
-	BIND_ADDRESS    = "BIND_ADDRESS"
-	ENV             = "ENV"
-	PORT            = "PORT"
-	REDIS_DB        = "REDIS_DB"
-	REDIS_HOST      = "REDIS_HOST"
-	REDIS_PASS      = "REDIS_PASS"
-	REDIS_PORT      = "REDIS_PORT"
-	SENTRY_DEBUG    = "SENTRY_DEBUG"
-	SENTRY_DSN      = "SENTRY_DSN"
-	TRACING_NAME    = "TRACING_NAME"
-	VERSION         = "VERSION"
-	ZIPKIN_ENDPOINT = "ZIPKIN_ENDPOINT"
+	BIND_ADDRESS      = "BIND_ADDRESS"
+	ENV               = "ENV"
+	INGRESS_BASE_PATH = "INGRESS_BASE_PATH"
+	INGRESS_HOST      = "INGRESS_HOST"
+	INGRESS_SCHEMES   = "INGRESS_SCHEMES"
+	PORT              = "PORT"
+	REDIS_DB          = "REDIS_DB"
+	REDIS_HOST        = "REDIS_HOST"
+	REDIS_PASS        = "REDIS_PASS"
+	REDIS_PORT        = "REDIS_PORT"
+	SENTRY_DEBUG      = "SENTRY_DEBUG"
+	SENTRY_DSN        = "SENTRY_DSN"
+	TRACING_NAME      = "TRACING_NAME"
+	VERSION           = "VERSION"
+	ZIPKIN_ENDPOINT   = "ZIPKIN_ENDPOINT"
 )
 
 // Value constants
