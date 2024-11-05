ca42efb1ba1a5cdb19ab11e657e0d75ed01ad261
Paul Schifferer <paul@schifferers.net>
2024-11-04 20:51:30 -0800

    Substructures

--- util/options.go
+++ util/options.go
@@ -9,9 +9,27 @@ import (
 	options "go.jtlabs.io/query"
 )
 
+type Sort struct {
+	Field string
+	Order int
+}
+
+type Filter struct {
+	Field     string
+	Operation interface{}
+}
+
+type Projection struct {
+	Field     string
+	Inclusion bool
+}
+
 type QueryParams struct {
-	Start int
-	Limit int
+	Start      int
+	Limit      int
+	Sort       []Sort
+	Filter     []Filter
+	Projection []Projection
 }
 
 func GetQueryParams(query string) QueryParams {
@@ -20,9 +38,27 @@ func GetQueryParams(query string) QueryParams {
 	opt, _ := options.FromQuerystring(query)
 	logging.Logger.Debug("options from query string", "opt", opt)
 
+	var sortFields []Sort // bson.D
+	for _, v := range opt.Sort {
+		sortFields = append(sortFields, Sort{v, 1} /*bson.E{v, 1}*/)
+	}
+
+	var filters []Filter // bson.D
+	for k, v := range opt.Filter {
+		filters = append(filters, Filter{k, v} /*bson.E{k, v}*/)
+	}
+
+	var proj []Projection // bson.D
+	for _, v := range opt.Fields {
+		proj = append(proj, Projection{v, true} /*bson.E{v, 1}*/)
+	}
+
 	params := QueryParams{
-		Start: int(math.Max(0, float64(opt.Page[constants.PageStartOption]))),
-		Limit: int(math.Max(1, math.Min(float64(dbconstants.QueryMaxSize), float64(opt.Page[constants.PageLimitOption])))),
+		Start:      int(math.Max(0, float64(opt.Page[constants.PageStartOption]))),
+		Limit:      int(math.Max(1, math.Min(float64(dbconstants.QueryMaxSize), float64(opt.Page[constants.PageLimitOption])))),
+		Sort:       sortFields,
+		Filter:     filters,
+		Projection: proj,
 	}
 
 	logging.Logger.Debug("final params", "params", params)
